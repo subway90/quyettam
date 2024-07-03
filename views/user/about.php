b182f48e309b68487b4db903a05f1b04f92fa470
subway90 <nguyenhieu3105@gmail.com>
2024-07-03 11:53:42 +0700

    about

--- views/user/about.php
+++ views/user/about.php
@@ -1,1 +1,96 @@
-Nội dung trang về chúng tôi
+<div class="h2 text-center text-danger text-uppercase mt-3">
+    về chúng tôi
+</div>
+
+<div class="row d-flex">
+    <div class="col-12 border-bottom border-danger text-uppercase text-danger text-start h3 mt-5 mb-3">
+        mục đích
+    </div>
+    <div class="col-12 col-md-6 col-lg-7 pe-0 pe-lg-5">
+        <p class="fs-5 lh-lg">
+            cộng đồng từ thiện <span class="text-danger">Quyết Tâm</span> được xây dựng nhằm mục đích kêu gọi ủng hộ để
+            tạo chiến dịch gây quỹ thiện nguyện. Dự án thiện nguyện đầu tiên là <a href="#"
+                class="text-active nav-link d-inline">Hỗ trợ học bổng cho bé Lê Thị Thu An</a>. Việc thiện nguyện sẻ
+            thông qua 2 hình thức chính là mua sản phẩm & quyên góp.
+        </p>
+        <a class="fst-italic nav-link text-hover" href="<?= URL ?>/tin-tuc/ho-tro-thien-nguyen">(đọc chi tiết)
+            &rarr;</a>
+    </div>
+    <div class="col-12 col-md-6 col-lg-5">
+        <img class="w-100" src="<?= URL ?>publics/image/1-800x600.jpg" alt="">
+    </div>
+</div>
+
+<div class="mt-5 row d-flex flex-row-reverse">
+    <div class="col-12 border-bottom border-danger text-uppercase text-danger text-end h3 mt-5 mb-3">
+        Ý NGHĨA
+    </div>
+    <div class="col-12 col-md-6 col-lg-7 pe-0 pe-lg-5">
+        <p class="fs-5 lh-lg">
+            Cộng đồng từ thiện chúng tôi theo tiêu chí <i>"Cho đi là còn mãi"</i>: cùng nhau sẻ chia với các hoàn cảnh
+            khó khăn, tạo cho họ cơ hội để có một cuộc sống tốt đẹp hơn. Và lan tỏa về những điều tích cực và tốt đẹp.
+        </p>
+    </div>
+    <div class="col-12 col-md-6 col-lg-5">
+        <img class="w-100" src="<?= URL ?>publics/image/istockphoto-1353332258-612x612.jpg" alt="">
+    </div>
+</div>
+
+<div class="row d-flex">
+    <div class="col-12 border-bottom border-danger text-uppercase text-danger text-start h3 mt-5 mb-3">
+        logo
+    </div>
+    <div class="col-12 col-md-6 col-lg-7 pe-0 pe-lg-5">
+        <p class="fs-5 lh-lg">
+            Ý nghĩa: 6 bàn tay của 6 con người, chính là 6 thành viên của nhóm <span class="text-danger">Quyết
+                Tâm</span>. màu đỏ tượng trưng cho sự nổi bật và nhiệt huyết. 6 bàn tay hình nấm đấm tượng trưng cho sự
+            <span class="text-danger">Quyết Tâm</span> và cùng hướng vào nhau có ý nghĩa là đồng hành cùng nhau và hướng
+            chung một mục đích.
+        </p>
+    </div>
+    <div class="col-12 col-md-6 col-lg-5">
+        <img class="w-75 float-end" src="<?= URL ?>publics/image/logo_qt_png.png" alt="">
+    </div>
+</div>
+
+<div class="mt-5 row d-flex flex-row-reverse">
+    <div class="col-12 border-bottom border-danger text-uppercase text-danger text-end h3 mt-5 mb-3">
+        thành viên
+    </div>
+    <div class="col-12 mt-3">
+        <div class="row d-flex justify-content-center">
+            <div class="col-4 col-md-4 col-lg-2">
+                <img src="<?= URL ?>publics/image/a1.png" alt="ảnh thành viên nhóm quyết tâm"
+                    class="w-100 rounded-circle">
+                <div class="fs-6 mt-3 text-danger text-center">Nhật Tân</div>
+            </div>
+            <div class="col-4 col-md-4 col-lg-2">
+                <img src="<?= URL ?>publics/image/a2.png" alt="ảnh thành viên nhóm quyết tâm"
+                    class="w-100 rounded-circle">
+                <div class="fs-6 mt-3 text-danger text-center">Mạnh Hùng</div>
+            </div>
+            <div class="col-4 col-md-4 col-lg-2">
+                <img src="<?= URL ?>publics/image/a3.png" alt="ảnh thành viên nhóm quyết tâm"
+                    class="w-100 rounded-circle">
+                <div class="fs-6 mt-3 text-danger text-center">Ngọc Hiếu</div>
+            </div>
+            <div class="col-4 col-md-4 col-lg-2">
+                <img src="<?= URL ?>publics/image/a4.png" alt="ảnh thành viên nhóm quyết tâm"
+                    class="w-100 rounded-circle">
+                <div class="fs-6 mt-3 text-danger text-center">Anh Cường</div>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center mt-5">
+            <div class="col-4 col-md-4 col-lg-2">
+                <img src="<?= URL ?>publics/image/a5.png" alt="ảnh thành viên nhóm quyết tâm"
+                    class="w-100 rounded-circle">
+                <div class="fs-6 mt-3 text-danger text-center">Phát Đạt</div>
+            </div>
+            <div class="col-4 col-md-4 col-lg-2">
+                <img src="<?= URL ?>publics/image/a6.png" alt="ảnh thành viên nhóm quyết tâm"
+                    class="w-100 rounded-circle">
+                <div class="fs-6 mt-3 text-danger text-center">Minh Hiếu</div>
+            </div>
+        </div>
+    </div>
+</div>
