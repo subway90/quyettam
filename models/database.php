57a14e63559d8d43b1afd6b181e219a8da303bab
subway90 <nguyenhieu3105@gmail.com>
2024-10-18 17:12:05 +0700

    update database model

--- models/database.php
+++ models/database.php
@@ -85,7 +85,6 @@ function pdo_query_one($sql)
  * Thực thi câu lệnh sql truy vấn một giá trị
  * @param string $sql câu lệnh sql
  * @param array $args mảng giá trị cung cấp cho các tham số của $sql
- * @return giá trị
  * @throws PDOException lỗi thực thi câu lệnh
  */
 function pdo_query_value($sql)
