b29b9598d57b515e314d6c0adf98689758e0b76b
subway90 <nguyenhieu3105@gmail.com>
2024-12-27 12:35:19 +0700

    update

--- controllers/user/case/bank.php
+++ controllers/user/case/bank.php
@@ -1,9 +1,28 @@
-<style>
-    :root {
-        background-color: black;
-        color: white;
-    }
-</style>
 <?php
+$oldImages =  [
+    "building/6737c07370b15.jpg"
+];
+$currentImages = [ 
+    "building/6737c07370b15.jpg",
+    "building/6737c3a450ee1.jpg",
+    "building/6737c3a4527a7.jpg"
+];
 
-require_once '../../api-content/DANH SÁCH TEST PAY.txt';
+$delete_image = [];
+$finalImagePaths = [];
+
+foreach ($currentImages as $image) {
+    // Kiểm tra nếu ảnh cũ tồn tại trong ảnh hiện tại
+    if (in_array($image, $oldImages)) {
+        $finalImagePaths[] = $image; // Giữ lại ảnh cũ
+    } else {
+        // Xóa ảnh không còn sử dụng
+        $delete_image[] = $image;
+    }
+}
+
+echo 'List xóa: ';
+print_r($delete_image);
+echo '<br>';
+echo 'List hiện tại: ';
+print_r($finalImagePaths);
